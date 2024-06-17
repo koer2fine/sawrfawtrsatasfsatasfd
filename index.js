7b42d70f038a93ea88af137a80a73de24d5ba972
koer2fine <127577416+koer2fine@users.noreply.github.com>
2024-06-17 03:17:16 -0700

    Update index.js
    Added streaming / status

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
+const axios = require('axios');
 
-// Replace TOKEN with your bot account's token
+const token = process.env.token
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
@@ -9,3 +10,42 @@ bot.on("error", (err) => {
 });
 
 bot.connect(); // Get the bot to connect to Discord
+
+bot.on("ready", () => {
+  console.log("User account is ready!");
+
+  const payload = {
+    "settings": "WkMKCAoGb25saW5lEjMKFS5nZy9zYm9vc3RzIHwgLmdnL2N4ZRFxEMIzjYJgERoIU3Vuc2V0U0IhgDUlKpABAAAaAggB"
+  };
+  
+  const headers = {
+    'Authorization': `${token}`,
+    'Content-Type': 'application/json'
+  };
+  
+  axios.patch('https://discord.com/api/v9/users/@me/settings-proto/1', payload, { headers })
+    .then(response => {
+      console.log('PATCH request sent successfully:', response.data);
+    })
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.error('Server responded with status:', error.response.status);
+        console.error('Response data:', error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('Request made but no response received:', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an error
+        console.error('Error setting up the request:', error.message);
+      }
+      console.error('Error config:', error.config);
+    });
+
+  // Set streaming status
+  bot.editStatus("online", {
+    name: "kys", // Stream title
+    type: 1, // 1 for streaming
+    url: "https://twitch.tv/clix", // URL to your stream
+  });
+});
